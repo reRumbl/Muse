e2f36348f8f71df34f02fe5533a29125f05c10fe
Balashov Igor <igor2233balashov@gmail.com>
2025-09-06 11:45:26 +0300

    Init commit: Base project structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="bg-black">
+            <h1 className="text-white">
+                Muse - музыкальный магазин виниловых пластинок.
+            </h1>
+        </div>
+    );
+}

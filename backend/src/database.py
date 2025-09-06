3b8152d854cd37658ec2cc907dcb962f7dee6440
Balashov Igor <igor2233balashov@gmail.com>
2025-09-06 16:00:06 +0300

    Create database engine, session factory and declarative base

---
+++ backend/src/database.py
@@ -0,0 +1,28 @@
+from uuid import UUID, uuid4
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
+from src.config import db_settings
+
+# --- Engine ---
+engine = create_async_engine(
+    url=db_settings.asyncpg_url,
+    pool_size=db_settings.DB_POOL_SIZE,
+    max_overflow=db_settings.DB_MAX_OVERFLOW,
+    echo=db_settings.DB_ECHO
+)
+
+# --- Session Factory ---
+SessionFactory = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
+)
+
+
+# --- DeclarativeBase Class ---
+class Base(AsyncAttrs, DeclarativeBase):
+    id: Mapped[UUID] = mapped_column(
+        default_factory=uuid4, 
+        primary_key=True, 
+        unique=True
+    )

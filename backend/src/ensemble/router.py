ef7de1878bdd7d2fab50ed1776013cbc9286800c
Balashov Igor <igor2233balashov@gmail.com>
2025-09-08 13:10:15 +0300

    Create router for ensemble and add new schema to base schemas

---
+++ backend/src/ensemble/router.py
@@ -0,0 +1,49 @@
+from uuid import UUID
+from fastapi import APIRouter
+from src.ensemble.dependencies import EnsembleServiceDep
+from src.ensemble.schemas import EnsembleCreate, EnsembleUpdate, Ensemble
+from src.schemas import SuccessResponse, CountResponse
+
+router = APIRouter(prefix='/ensembles', tags=['ensemble'])
+
+
+@router.post('/', response_model=Ensemble)
+async def create_ensemble(
+    ensemble_data: EnsembleCreate, 
+    service: EnsembleServiceDep
+):
+    return await service.create(ensemble_data)
+
+
+@router.get('/{ensemble_id}', response_model=Ensemble)
+async def get_ensemble(
+    ensemble_id: UUID,
+    service: EnsembleServiceDep
+):
+    return await service.get(ensemble_id)
+
+
+@router.get('/{ensemble_id}/compositions/count', response_model=CountResponse)
+async def get_ensemble_compositions_count(
+    ensemble_id: UUID,
+    service: EnsembleServiceDep
+):
+    compositions = await service.get_all_compositions(ensemble_id)
+    return CountResponse(count=len(compositions))
+    
+
+@router.put('/{ensemble_id}/update', response_model=Ensemble)
+async def update_ensemble(
+    ensemble_id: UUID,
+    ensemble_data: EnsembleUpdate,
+    service: EnsembleServiceDep
+):
+    return await service.update(ensemble_id, ensemble_data)
+
+
+@router.delete('/{ensemble_id}/delete', response_model=SuccessResponse)
+async def delete_ensemble(
+    ensemble_id: UUID,
+    service: EnsembleServiceDep
+):
+    return await service.delete(ensemble_id)
--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 
 class SuccessResponse(BaseModel):
     message: str
+
+
+class CountResponse(BaseModel):
+    count: int

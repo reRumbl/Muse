0833db01075cbf2fcd75d591f5fa9276818a8a48
Balashov Igor <igor2233balashov@gmail.com>
2025-09-11 11:34:41 +0300

    Include all routers in fastapi app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from src.schemas import SuccessResponse
+from src.company.router import router as company_router
+from src.composition.router import router as composition_router
 from src.ensemble.router import router as ensemble_router
+from src.musician.router import router as musician_router
+from src.performance.router import router as performance_router
+from src.record.router import router as record_router
+from src.release.router import router as release_router
 
 # --- FastAPI App ---
 app = FastAPI(
@@ -12,7 +18,13 @@ app = FastAPI(
 )
 
 # --- Routers ---
+app.include_router(company_router)
+app.include_router(composition_router)
 app.include_router(ensemble_router)
+app.include_router(musician_router)
+app.include_router(performance_router)
+app.include_router(record_router)
+app.include_router(release_router)
 
 
 # --- Root Endpoint ---

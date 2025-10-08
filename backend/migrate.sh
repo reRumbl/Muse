55481ab6feb93c52a94671eb7d8f3b05062bcbd9
Balashov Igor <igor2233balashov@gmail.com>
2025-10-08 10:00:28 +0300

    Update migration script

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 # migrate.sh
 
-# Настройки подключения к БД
-HOST=$DB_HOST
-PORT=$DB_PORT
-
-# Ожидание готовности базы данных
-echo "Waiting for PostgreSQL ($HOST:$PORT) to be ready..."
-
-# Цикл ожидания: повторять до тех пор, пока не будет установлена связь
-until nc -z $HOST $PORT; do
-  echo "PostgreSQL is unavailable - sleeping"
-  sleep 1
-done
-
-echo "PostgreSQL is up - executing command"
 
 # 1. Запуск миграций Alembic
 alembic upgrade head
